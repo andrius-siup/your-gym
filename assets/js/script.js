6e810c75f01684b8fef0644eed898bd09c7e7b1b
andrius-siup <andrius.siupinys@gmail.com>
2021-11-28 15:21:29 +0000

    Make mobile navigation active

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,11 @@
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+// Mobile navigation
+const btnNav = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header");
+
+btnNav.addEventListener("click", function () {
+  header.classList.toggle("nav-open");
+});
